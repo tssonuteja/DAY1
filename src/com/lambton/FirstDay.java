744dbccd37feabc82dcd4c0947b95225487bad89
macstudent <saitejasunkari04@gmail.com>
2019-09-18 19:21:31 -0400

    [Saiteja] comment added

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -4,9 +4,12 @@ public class FirstDay {
   public static void main(String[] args)
   {
       System.out.println("saiteja");
+      //Variable declaration
       int a,b,c;
+      //Value assignment
       a=2;
       b=3;
+      //Perform Add
       c=a+b;
       System.out.println("sum : "+c);
 
