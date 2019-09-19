91232df4f98323ef2523fe836e5b89745dd3a739
macstudent <saitejasunkari04@gmail.com>
2019-09-18 21:00:24 -0400

    [Saiteja] Studentid,name and loops

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -9,10 +9,26 @@ public class FirstDay {
       //Value assignment
       a=2;
       b=3;
-      
+
       c=a+b;
       System.out.println("sum : "+c);
+      int x= Integer.MAX_VALUE;
+      System.out.println("x : "+x);
 
+      int y=x+1;
+      System.out.println(y);
+
+      System.out.println("MIN : "+Integer.MIN_VALUE);
+
+      SaiTeja s1 =new SaiTeja();
+      s1.setValues();
+      s1.display();
+
+
+       for(int i=1;i<10;i++)
+       {
+           System.out.println("SAITEJA");
+       }
 
   }
 }
--- src/com/lambton/SaiTeja.java
+++ src/com/lambton/SaiTeja.java
@@ -3,4 +3,15 @@ package com.lambton;
 public class SaiTeja {
     int studentId;
     String studentName;
+
+    void setValues()
+    {
+        studentId=1;
+        studentName ="Saiteja";
+    }
+    void display()
+    {
+        System.out.println("student Id : " +studentId);
+        System.out.println("student Name : "+studentName);
+    }
 }
